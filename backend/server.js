aed33a944e4d2f24332747dbf1b45012647638f9
bibekstha123 <bibekstha084@gmail.com>
2023-08-11 22:28:32 +0930

    working on integrating api

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ var cookieParser = require("cookie-parser");
 var connectDb = require("./config/db");
 const port = process.env.PORT || 5000;
 var userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 connectDb();
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
---
+++ frontend/src/slices/userApiSlice.jsx
@@ -0,0 +1,16 @@
+import { apiSlice } from "./apiSlice";
+const USERS_URL = "http://localhost:5000/api/users";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = userApiSlice;
